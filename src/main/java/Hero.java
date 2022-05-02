30574a61dda777861ac80e55081e0bcdf066a2a5
Nicholas-Kimutai <kimutainick74@gmail.com>
2022-05-03 01:57:03 +0300

    Add a Hero constructor

--- src/main/java/Hero.java
+++ src/main/java/Hero.java
@@ -1,2 +1,15 @@
+import java.util.ArrayList;
+
 public class Hero {
+    private int age;
+    private String name;
+    private ArrayList<String>powers =new ArrayList<String>();
+    private ArrayList<String>weaknesses=new ArrayList<String>();
+
+    public Hero (String name,int age,ArrayList<String>powers,ArrayList<String>weaknesses){
+        this.age=age;
+        this.name=name;
+        this.powers=powers;
+        this.weaknesses=weaknesses;
+    }
 }
