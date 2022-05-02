04248f61d0eba4895d8e3cdd6b6635f3eb54f68a
Nicholas-Kimutai <kimutainick74@gmail.com>
2022-05-03 02:30:13 +0300

    Create a get name method

--- src/main/java/Hero.java
+++ src/main/java/Hero.java
@@ -1,15 +1,25 @@
 import java.util.ArrayList;
 
 public class Hero {
-    private int age;
     private String name;
+    private int age;
+    private int id;
     private ArrayList<String>powers =new ArrayList<String>();
     private ArrayList<String>weaknesses=new ArrayList<String>();
+    private static ArrayList<Hero> instances = new ArrayList<Hero>();
 
     public Hero (String name,int age,ArrayList<String>powers,ArrayList<String>weaknesses){
-        this.age=age;
         this.name=name;
+        this.age=age;
         this.powers=powers;
         this.weaknesses=weaknesses;
+        instances.add(this);
+        this.id = instances.size();
     }
+    // create get methods
+
+    public String getName(){
+        return name;
+    }
+
 }
--- src/test/java/HeroTest.java
+++ src/test/java/HeroTest.java
@@ -1,2 +1,16 @@
+import org.junit.After;
+import org.junit.Before;
+
+import java.util.ArrayList;
+
 public class HeroTest {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
 }
