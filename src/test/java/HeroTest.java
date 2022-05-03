d5c01e013a0460ee1c433bdece4a5937ae9c0a51
Nicholas-Kimutai <kimutainick74@gmail.com>
2022-05-04 00:41:28 +0300

    update tests

--- src/test/java/HeroTest.java
+++ src/test/java/HeroTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import java.util.ArrayList;
 
 import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertTrue;
 
 public class HeroTest {
     @Before
@@ -15,10 +16,63 @@ public class HeroTest {
     public void tearDown() throws Exception {
 
     }
-    @Test
-    public void NewHeroObjectCorrectlyCreated_true() throws Exception{
-        Hero myHero = new Hero("Nick", 25, "Fast thinker", "Impatience");
-        assertEquals(true, myHero instanceof Hero);
+   @Test
+   public void NewHeroObjectGetsCorrectlyCreated_true() throws Exception{
+       ArrayList<String> powers = new ArrayList<String>();
+       ArrayList<String> weaknesses = new ArrayList<String>();
+       powers.add("fly");
+       powers.add("super strength");
+       weaknesses.add("Impatience");
+       weaknesses.add("Lazy");
 
+       Hero hero = new Hero("Nick",25,powers,weaknesses,1);
+       assertEquals(true, hero instanceof Hero);
+   }
+    public Hero setupNewHero() throws Exception{
+        ArrayList<String> powers = new ArrayList<String>();
+        ArrayList<String> weaknesses = new ArrayList<String>();
+        powers.add("Fly");
+        powers.add("super strength");
+        weaknesses.add("Impatience");
+        weaknesses.add("Lazy");
+
+        Hero hero = new Hero("Nick",25,powers,weaknesses,1);
+        return  hero;
     }
+
+    public Hero setupAnotherHero() throws Exception{
+        ArrayList<String> powers = new ArrayList<String>();
+        ArrayList<String> weaknesses = new ArrayList<String>();
+        powers.add("Fly");
+        powers.add("super strength");
+        weaknesses.add("Impatience");
+        weaknesses.add("Lazy");
+
+        Hero anotherHero = new Hero("Spider",30,powers,weaknesses,1);
+
+        return  anotherHero;
+    }
+
+   @Test
+   public void HeroInstanceWithPowers_true() throws Exception{
+       Hero hero = setupNewHero();
+       assertTrue(hero.getPowers().contains("Fly"));
+   }
+    @Test
+    public void HeroInstanceWithName_true() throws Exception{
+        Hero hero = setupNewHero();
+        assertEquals("Nick",hero.getName());
+    }
+
+    @Test
+    public void HeroInstanceWithAge_true() throws Exception{
+        Hero hero = setupNewHero();
+        assertEquals(25,hero.getAge());
+    }
+    @Test
+    public void HeroInstanceWithWeaknesses_true() throws Exception{
+        Hero hero = setupNewHero();
+        assertTrue(hero.getWeaknesses().contains("Lazy"));
+    }
+
 }
--- src/test/java/SquadTest.java
+++ src/test/java/SquadTest.java
@@ -12,10 +12,25 @@ public class SquadTest {
     @After
     public void tearDown() throws Exception {
     }
+    public Squad setupSquad(){
+        return new Squad(2, "Vybo", "Fight cybercrime");
+    }
 
     @Test
     public void NewSquadObjectGetsCorrectlyCreated_true() throws Exception{
-        Squad squad = new Squad(10, "Titans", "Fight cybercrime");
+        Squad squad = new Squad(2, "Vybo", "Fight cybercrime");
         assertEquals(true, squad instanceof Squad);
     }
+    @Test
+    public void SquadInstantiatesWithMaxSize_true() {
+        Squad squad = setupSquad();
+        assertEquals(2, squad.getMaxSize());
+    }
+
+    @Test
+    public void SquadInstantiatesWithName_true() {
+        Squad squad = setupSquad();
+        assertEquals("Vybo", squad.getName());
+    }
+
 }
