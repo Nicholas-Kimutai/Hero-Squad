2f6935ae00af2581e1c984629c2c5d7b3b5996c3
Nicholas-Kimutai <kimutainick74@gmail.com>
2022-05-03 04:09:09 +0300

    Create Squad test case

--- src/test/java/SquadTest.java
+++ src/test/java/SquadTest.java
@@ -1,2 +1,21 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
 public class SquadTest {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void NewSquadObjectGetsCorrectlyCreated_true() throws Exception{
+        Squad squad = new Squad(10, "Titans", "Fight cybercrime");
+        assertEquals(true, squad instanceof Squad);
+    }
 }
