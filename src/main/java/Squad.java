8028c0817383337a0ba24bfadd8fa7a79af1d3e0
Nicholas-Kimutai <kimutainick74@gmail.com>
2022-05-03 15:14:33 +0300

    refactor Squad class

--- src/main/java/Squad.java
+++ src/main/java/Squad.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.List;
 
 public class Squad {
     private int maxSize;
@@ -6,6 +7,7 @@ public class Squad {
     private String cause;
     private static ArrayList<Squad> instance = new ArrayList<Squad>();
     private int id;
+    private List<Hero> Heroes;
 
 // Create a constructor
     public Squad(int maxSize, String name, String cause){
@@ -14,10 +16,11 @@ public class Squad {
         this.cause = cause;
         instance.add(this);
         this.id = instance.size();
+        Heroes=new ArrayList<Hero>();
 
     }
 
-    // Create get and set methods
+    // Create get methods
     public int getMaxSize() {
         return maxSize;
     }
@@ -30,14 +33,30 @@ public class Squad {
     public static  ArrayList<Squad> getAll(){
         return instance;
     }
-    public void setId(int id) {
-        this.id = id;
-    }
 
     public int getId() {
         return id;
     }
     public static Squad findById(int id) {
         return instance.get(id - 1);
+    }
+    public List<Hero> getHeroes() {
+        return Heroes;
+    }
+
+    public void addHero(Hero hero) {
+        Heroes.add(hero);
+    }
+
+    public static boolean heroAlreadyExists(Hero newHero) {
+        boolean exists = false;
+        for (Squad squad : instance) {
+            for (Hero hero : squad.getHeroes()) {
+                if (hero.getName().equals(newHero.getName())) {
+                    exists = true;
+                }
+            }
+        }
+        return exists;
     }
 }
