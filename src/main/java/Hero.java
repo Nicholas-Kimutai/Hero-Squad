577e4af60e5b2db668e754e4e026ec4160a45bfd
Nicholas-Kimutai <kimutainick74@gmail.com>
2022-05-03 21:35:32 +0300

    update app

--- src/main/java/Hero.java
+++ src/main/java/Hero.java
@@ -3,12 +3,13 @@ import java.util.ArrayList;
 public class Hero {
     private String name;
     private int age;
-    private String powers;
-    private String weaknesses;
     private int id;
+    private ArrayList<String> powers = new ArrayList<String>();
+    private ArrayList<String> weaknesses = new ArrayList<String>();
     private static ArrayList<Hero> instances = new ArrayList<Hero>();
+    private int squadId;
 
-    public Hero (String name,int age,String powers,String weaknesses){
+    public Hero (String name,int age,ArrayList<String> powers,ArrayList<String> weaknesses, int squadId){
         this.name=name;
         this.age=age;
         this.powers=powers;
@@ -26,12 +27,12 @@ public class Hero {
         return age;
     }
 
-    public String getPowers() {
+    public ArrayList<String> getPowers() {
         return powers;
     }
 
 
-    public String getWeaknesses() {
+    public ArrayList<String> getWeaknesses() {
         return weaknesses;
     }
 
@@ -42,7 +43,7 @@ public class Hero {
     public static ArrayList<Hero> getAll(){
         return instances;
     }
-    public static void clear(){
+    public static void clearAll(){
         instances.clear();
     }
 
@@ -50,6 +51,21 @@ public class Hero {
     public static Hero findById(int id){
         return  instances.get(id-1);
     }
+    public void deleteHero(){
+        instances.remove(id-1);
+    }
+    public void update(String name, int age, ArrayList<String>  powers, ArrayList<String> weaknesses){
+        this.name = name;
+        this.age = age;
+        this.powers = powers;
+        this.weaknesses = weaknesses;
+    }
+
+
+    public int getSquadId() {
+        return squadId;
+    }
+
 
 }
 
